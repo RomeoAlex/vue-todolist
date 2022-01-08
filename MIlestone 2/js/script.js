747e3209e5988a96104bd1a5ae838c6bfdfe8401
RomeoAlex <romeo.alessandro17@gmail.com>
2022-01-08 20:02:42 +0100

    riorganizzato le cartelle per milestone e correzioni

--- MIlestone 2/js/script.js
+++ MIlestone 2/js/script.js
@@ -3,10 +3,9 @@
 // Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 // - text, una stringa che indica il testo del todo
 // - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all'interno di una lista, un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 Vue.config.devtools = true;
 const app = new Vue(
     {
@@ -37,6 +36,10 @@ const app = new Vue(
             ],
         },
         methods:{
+            removeTodo: function(index) {
+                this.todoList.splice(index, 1);
+            }
+
         }
     }
 );
---
+++ Milestone 3/js/script.js
@@ -0,0 +1,54 @@
+
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+Vue.config.devtools = true;
+const app = new Vue(
+    {
+        el:'#root',
+        data: {
+            newTodo:'',
+            todoList: [
+                {
+                    text:'Fare la spesa',
+                    done:false
+                },
+                {
+                    text:'Portare fuori il cane',
+                    done:true
+                },
+                {
+                    text:'Preparare il caffè',
+                    done:true
+                },
+                {
+                    text:'Fare gli esercizi',
+                    done:false
+                },
+                {
+                    text:'Chiamare l\'idraulico',
+                    done:false
+                }, 
+            ],
+        },
+        methods:{
+            removeTodo: function(index) {
+                this.todoList.splice(index, 1);
+            },
+            addTodo: function(){
+                // utilizzo trim per rimuovere gli spazi
+                const newTrimTodo = this.newTodo.trim();
+                if(newTrimTodo.length > 0 )
+                // devo pushare un oggetto
+                this.todoList.push({
+                    text:newTrimTodo,
+                    done:false,
+                });
+                // riporto input vuoto dopo aver inserito un nuovo todo
+                this.newTodo =''; 
+            },
+        }
+    }
+);
